3b42445aabbc2b95ed7ec46096eeea9b67268a0c
zero2pi <prasanjit.thats.me@gmail.com>
2020-03-19 18:41:02 +0530

    changes

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
  _#!/bin/bash_
 
 export PM2_HOME=/home/ubuntu/.pm2  
-pm2 devops-test 
-cd /home/ubuntu/simple-express/  
-pm2 start --name devops-test npm -- start
+pm2 delete devops-test 
+cd /home/ubuntu/devops-test/dist/
+pm2 start main.js --name devops-test
